0ec0e859d9a5df9925a72b74ed5fe924d9300217
Mike Hingley <computa_mike@hotmail.com>
2021-12-06 11:30:21 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace NotifyMe
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(535, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "This is a sample Program... Every thime you click the form...";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(306, 209);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(243, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "A Notification is displayed";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(561, 243);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.MinimumSize = new System.Drawing.Size(577, 282);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Click += new System.EventHandler(this.Form1_Click);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotifyMe
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			SetBalloonTip();
+		}
+
+
+
+
+
+		private void SetBalloonTip()
+		{
+			notifyIcon1.Icon = SystemIcons.Application;
+			notifyIcon1.BalloonTipTitle = "This is a notification";
+			notifyIcon1.BalloonTipText = " Notifications can become distracting when you are recording a screen cast.";
+			notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+			this.Click += new EventHandler(Form1_Click);
+		}
+
+		void Form1_Click(object sender, EventArgs e)
+		{
+			notifyIcon1.Visible = true;
+			notifyIcon1.ShowBalloonTip(30000);
+		}
+	}
+}
